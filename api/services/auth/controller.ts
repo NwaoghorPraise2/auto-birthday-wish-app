9f45bf690a3e9dc2d787426db8fb3351f4f1d08c
Nwaoghor Praise Chukunweiken <chukunwaoghor@gmail.com>
2024-07-08 22:30:08 +0100

    Delete unsed file

--- api/services/auth/controller.ts
+++ api/services/auth/controller.ts
@@ -3,26 +3,7 @@ import db from '../../config/db';
 
 
 const register = (res:Response, req: Request): void=> {
-    try {
-        const { username, email, password } = req.body;
-        db.query(
-            "INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *",
-            [username, email   assword],
-            (err, result) => {
-              if (err) {
-                console.error(err);
-                res.status(500).send("Error registering user");
-              } else {
-                res.status(201).json({
-                  message: "User registered successfully",
-                  user: result.rows[0],
-                });
-              }
-            }
-          );
-    } catch (error) {
-        console.log(error)
-    }
+   
 }
 
 
