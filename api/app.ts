d36772f02ea034a2eaf2987c93cc30ca6bd270e3
Nwaoghor Praise Chukunweiken <chukunwaoghor@gmail.com>
2024-07-13 21:31:52 +0100

    Delete unsed file

--- api/app.ts
+++ api/app.ts
@@ -1,12 +1,16 @@
 import express, {Application, Request, Response} from 'express';
 import logger from 'morgan';
 import auth from './services/auth/route';
+import helmet from 'helmet';
+import cors from 'cors';
 
 const app: Application = express();
 
 // Logging middleware
 app.use(logger('dev'));
-
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
 // Route mounting
 app.use('/api/auth', auth);
