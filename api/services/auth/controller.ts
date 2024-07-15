4eb90884c167353ffb90e93af313066f679ff254
Nwaoghor Praise Chukunweiken <chukunwaoghor@gmail.com>
2024-07-15 21:31:13 +0100

    Delete unsed file

--- api/services/auth/controller.ts
+++ api/services/auth/controller.ts
@@ -1,9 +1,14 @@
 import { Request, Response } from 'express'
 import db from '../../config/db';
+import { User } from '@prisma/client';
 
 
-const register = (res:Response, req: Request): void=> {
-   
+const register = (res:Response, req: Request<User>): void=> {
+   //validate the input
+   //if error, throw error to user
+   //else, persist data to database
+   //sign token
+   // respond to user with a message
 }
 
 
---
+++ api/services/auth/interfaces/Auth.ts
@@ -0,0 +1,11 @@
+import * as z from 'zod';
+
+const User = z.object({
+  username: z.string(),
+  name: z.string().optional(),
+  email: z.string().email(),
+  phone_number: z.string().optional(),
+  password: z.string(),
+});
+
+export type User = z.infer<typeof User>;
