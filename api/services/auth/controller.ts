f90055e8d2acc73503714430ffa4084ae905403f
Nwaoghor Praise Chukunweiken <chukunwaoghor@gmail.com>
2024-07-15 21:39:18 +0100

    Delete unsed file

--- api/services/auth/controller.ts
+++ api/services/auth/controller.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from 'express'
 import db from '../../config/db';
-import { User } from '@prisma/client';
+import { User } from '@prisma/client'
 
 
-const register = (res:Response, req: Request<User>): void=> {
+const register = async (res:Response, req: Request<User>)=> {
+    // const insertedUser = await db.user.create({})
    //validate the input
    //if error, throw error to user
    //else, persist data to database
--- api/services/auth/route.ts
+++ api/services/auth/route.ts
@@ -9,11 +9,6 @@ router.get('/signup', register);
 router.post('/login', login);
 router.post('/change-password', changePassword);
 
-// router.post('/login', (req, res) => {
-//   res.json({
-//     code: 200,
-//     message: 'success'
-//   })
-// })
+
 
 export default router;
