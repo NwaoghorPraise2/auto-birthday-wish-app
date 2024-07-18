ee7882a9522a42f52b98cf517ef21e53cbe4e478
Nwaoghor Praise Chukunweiken <chukunwaoghor@gmail.com>
2024-07-18 21:17:24 +0100

    Feat: add zod for data validation

--- api/services/auth/controller.ts
+++ api/services/auth/controller.ts
@@ -5,7 +5,27 @@ import { User } from './interfaces/Auth';
 
 
 const register = async (res:Response, req: Request<User>)=> {
+    try{
+    const {email, password, name, phone_number} = req.body;
 
+    const isUserExist = await db.user.findUnique({
+        where: {
+            email
+        }
+    });
+
+    if (isUserExist) {
+        return res.json({
+            message: 'User already exist'
+        })
+    }
+    const insertedUser = await db.user.create({
+        data: req.body
+    })
+    return res.json(insertedUser)
+    }catch(e){
+        return res.json(e)
+    }
     // const result = User.parse(req.body);
     // const insertedUser = await db.user.create({})
    //validate the input
